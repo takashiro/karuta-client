a8216e47e11d1a5280ce73a88b1d542991d400cd
Kazuichi Takashiro <takashiro@qq.com>
2021-07-28 22:15:52 +0800

    Export *Profile and *Configuration

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ export {
 	Context,
 	ContextListener,
 	Connection,
+	UserProfile,
+	RoomProfile,
+	RoomConfiguration,
+	DriverProfile,
 } from '@karuta/core';
 export { default } from './Client';
 export { default as Driver } from './Driver';
